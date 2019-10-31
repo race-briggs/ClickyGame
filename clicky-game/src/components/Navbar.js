597188fda023ecb89f99d9de78d821d09d96e239
race-briggs <race.briggs@gmail.com>
2019-10-31 09:43:47 -0500

    added component files and began Navbar

---
+++ clicky-game/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Navbar() {
+  return (
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a className="navbar-brand" href="#"><bold>M:TG Clicker Game</bold></a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+  </nav>
+  )
+}
+
+export default Navbar;
